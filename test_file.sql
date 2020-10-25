a9461fa51aaac0075f8b073deb2dce603b0d9ca7
Ruben Coloma <coloma.ruben@gmail.com>
2020-10-24 22:52:23 -0600

    Se agrego el archivo de pruebas SQL con una consulta de clietnes vigentes

---
+++ test_file.sql
@@ -0,0 +1,8 @@
+--- Autor: Ruben Coloma
+--- Fecha Emision: 24/OCT/2020
+--- Razon: Curso Profesional de Git y GitHub
+
+-- Lista de Clientes
+SELECT  *
+FROM    dbo.tbl_cliente
+WHERE   cl_estado = 'V'
